f84a9d53dad58f4dea84d6faec7a52ab93dbda31
Thales Daniel <0xthalesx0@gmail.com>
2020-08-16 01:24:56 -0300

    Aula 10

---
+++ Curso Python 3/Aula 10 - Tipos de dados primitivos/aula10.py
@@ -0,0 +1,13 @@
+"""
+Essa aula explica sobre tipos de dados
+str - string - textos 'assim' "assim"
+int - inteiro - 123456 0 -10 200
+float - real/ponto flutuante - 10.50 1.5 0.0
+bool - booleano/lógico - True/False 10 == 10
+"""
+print('Luiz')
+print('Luiz', type('Luiz'))  # informa a classe da variavel (no caso, str)
+print('10', type('10'))  # o 10 é uma string, então math não funciona/dá problema
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
+print(20 == 10, type(20 == 10))
