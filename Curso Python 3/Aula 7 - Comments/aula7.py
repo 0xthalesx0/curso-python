4661be8476bfd5d3bfd6c596f52d241cc6d67240
Thales Daniel <0xthalesx0@gmail.com>
2020-08-16 00:31:29 -0300

    commit inicial

---
+++ Curso Python 3/Aula 7 - Comments/aula7.py
@@ -0,0 +1,11 @@
+"""
+print('Bruno Corno 1')"""
+print('Bruno Corno 2')
+# Falando sobre do que se trata essa linha
+# Uma linha
+# Outra linha
+print('Bruno Corno 3')
+# print('Bruno Corno 4')  # Isso é um comentário
+print('Bruno Corno 5')  # Mais uma
+print('Bruno Corno 6')
+# Final
---
+++ Curso Python 3/Aula 8 - print/aula8.py
@@ -0,0 +1,5 @@
+
+print(123456)
+print('Bruno', 'Corno', 'aa')
+print('Bruno', 'Muito', 'Corno', sep='-', end='\n')  # end define como terminar a linha
+print('Bruno', 'Muito', 'Corno', sep='-')
