a90ab06c5b5a9289b2ecabd946ef3dc5ee7184b1
Thales Daniel <0xthalesx0@gmail.com>
2020-08-16 01:09:42 -0300

    Aula 9 - Implementado com git

--- Curso Python 3/Aula 9 - Strings e Aspas/aula9.py
+++ Curso Python 3/Aula 9 - Strings e Aspas/aula9.py
@@ -5,3 +5,9 @@ str - string
 print('alcuma coisa')
 print("aspas duplas")
 print(10)
+
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str). \n')
+
+print("Essa é uma \"string\" (str).")
+print('Essa é uma \'string\' (str).')
