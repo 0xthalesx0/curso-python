f95da7a00fe4fcb97349336179fb936d08825a47
Thales Daniel <0xthalesx0@gmail.com>
2020-08-16 01:38:18 -0300

    Aula 10

---
+++ Curso Python 3/Aula 11 - Operadores Aritméticos/aula11.py
@@ -0,0 +1,24 @@
+"""
++ - adição
+- - subtração
+* - multiplicação
+/ - divisão
+//- divisão inteira
+**- exponenciação
+% - módulo/resto
+()- ordem
+"""
+
+print(10 * 10)
+print(10 + 10)
+print(10 - 5)
+print(10 / 10)
+print('\n')
+
+print('10' * 10)  # repetição de string
+print('10' + '10')  # concatenação
+print('Luiz Otávio tem ' + str(32) + ' anos')  # conversão str
+
+print(10 // 3)
+print(10 ** 10)
+print(10 % 3)
